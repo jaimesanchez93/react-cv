479d1d2ed031a820e8d99de685bb088b37f6affe
jaime <jaime.sanchez@thekeytalent.com>
2019-10-15 08:40:50 +0200

    Added react-i18next library (translations)

---
+++ src/i18n.js
@@ -0,0 +1,45 @@
+import i18n from "i18next";
+import {
+    initReactI18next
+} from "react-i18next";
+
+import detector from "i18next-browser-languagedetector";
+
+
+// the translations
+// (tip move them in a JSON file and import them)+
+
+import  translationEn from './translations/en.json';
+import  translationEs from './translations/es.json';
+
+const language = window.location.pathname.replace("/","");
+console.log('language', language);
+
+console.log('translation es',translationEs);
+
+
+const resources = {
+    en: {
+        translation: translationEn
+    },
+    es: {
+        translation: translationEs
+    }
+};
+
+i18n
+    .use(detector)
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        resources,
+        lng: "en",
+        fallbackLng: "en", // use en if detected lng is not available
+        keySeparator: false, // we do not use keys in form messages.welcome
+
+        interpolation: {
+            escapeValue: false // react already safes from xss
+        }
+    });
+
+
+export default i18n;
