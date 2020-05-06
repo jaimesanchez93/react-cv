5a8cb8518363148db3bdd8c61a8ceb8e7b67ed9a
jaime <jaimesanchez.93@gmail.com>
2020-05-06 16:21:23 +0200

    Updates

---
+++ precache-manifest.7461b9b220c11a552776c7d831c9cbe3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467768d2566dd4dba02738e88fa6bbec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96f933f092987bad767c",
+    "url": "/static/css/main.e7009728.chunk.css"
+  },
+  {
+    "revision": "3c86dd8ce2038286ff19",
+    "url": "/static/js/2.7b57429b.chunk.js"
+  },
+  {
+    "revision": "96f933f092987bad767c",
+    "url": "/static/js/main.59255370.chunk.js"
+  },
+  {
+    "revision": "3efac054ebaa19b62d7a",
+    "url": "/static/js/runtime~main.4b1521c4.js"
+  },
+  {
+    "revision": "2d0e4ec04dfa48efe45129c3cbf73c7b",
+    "url": "/static/media/Ellipse_5.2d0e4ec0.svg"
+  },
+  {
+    "revision": "21875577a8802a6351ceb697093d6213",
+    "url": "/static/media/Springer Ebooks.21875577.pdf"
+  },
+  {
+    "revision": "f4052974a4cc2a6596d3fada66bcc7da",
+    "url": "/static/media/american-football.f4052974.svg"
+  },
+  {
+    "revision": "e6ec5b85a6acdb325759878d096a504f",
+    "url": "/static/media/criptored.e6ec5b85.png"
+  },
+  {
+    "revision": "bef5e4ae3d0f1c584fe7841ca825453b",
+    "url": "/static/media/gamepad.bef5e4ae.svg"
+  },
+  {
+    "revision": "d5cb2e7af415fbdce5181bbc2110ed12",
+    "url": "/static/media/headphones.d5cb2e7a.svg"
+  },
+  {
+    "revision": "9cfdb47b59f605412f8158c7c55dfba0",
+    "url": "/static/media/kas_factory.9cfdb47b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "802ae12b5bc9260537e66fb8f76de984",
+    "url": "/static/media/popcorn.802ae12b.svg"
+  },
+  {
+    "revision": "b36310c2fba33b030433d159bdcc0288",
+    "url": "/static/media/the_key_talent.b36310c2.png"
+  }
+]);
